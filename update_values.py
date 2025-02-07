48718fbdc8a2ca55f6f38e9ec46e47c83b121bed
Abdullah <4144429+abdullah85@users.noreply.github.com>
2025-02-08 00:58:49 +0530

    Implemented caching to handle 429 issue.

--- update_values.py
+++ update_values.py
@@ -3,6 +3,7 @@ import json
 import requests
 import math
 import random
+import os
 
 use_random_values = False  # Set to False to use real data
 move_amount_high = 1  # Variable to control the amount the coins move by when high
@@ -14,15 +15,52 @@ liquidity_divisor = 100000000
 price_divisor = 1000
 engagements_divisor = 100000
 
+cookie_api_key = os.getenv("COOKIE_API_KEY")
+base_url = "https://api.cookie.fun"
+
+if not cookie_api_key:
+    raise ValueError("Cookie API key is missing. Please add it to the .env file.")
+
+class CachedPages:
+    def __init__(self, duration=5, threshold=600):
+        self.duration = duration
+        self.threshold = threshold
+        self.data = {}
+        self.initTime = {} # time to live info
+
+    def updateDuration(self):
+        self.duration *= 2
+        if(self.duration >= self.threshold):
+            self.duration = 5 # reset the time to wait
+
+    def set(self, page_number, response):
+        self.data[page_number] = response
+        self.initTime[page_number] = time.time()
+
+    def get(self, page_number):
+        if(page_number in self.initTime.keys()):
+            currDuration = time.time() - self.initTime[page_number]
+            if(page_number in self.data.keys() and currDuration <= self.duration):
+                return self.data[page_number]
+        url = f'https://api.cookie.fun/v2/agents/agentsPaged?interval=_7Days&page={page_number}&pageSize=25'
+        headers = {'x-api-key': cookie_api_key}
+        response = requests.get(url, headers=headers)
+        # Make the request and send the response and the onus is on the invoker to verify and set
+        return response
+
+pages = CachedPages()
+
 # Fetch data from CookieDAO API
-def fetch_data():
-    url = 'https://api.cookie.fun/v2/agents/agentsPaged?interval=_7Days&page=1&pageSize=25'
-    headers = {'x-api-key': 'b8356b68-33b6-487b-98e5-ccf0f0f2ced4'}
-    response = requests.get(url, headers=headers)
+def fetch_data(page_number=1):
+    response = pages.get(page_number)
     if response.status_code == 200:
+        pages.set(page_number, response)
         return response.json()['ok']['data']
-    else:
-        return []
+    # Update duration and log the error message
+    pages.updateDuration()
+    print(response)
+    # Return the empty list
+    return []
 
 cryptocurrencies = fetch_data()
 
